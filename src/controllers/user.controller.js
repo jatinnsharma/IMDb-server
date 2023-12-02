5c960efeaf87265a2fef5fb6c5c4db4364a8f0c2
jatin <jatinsharma2231@gmail.com>
2023-12-02 18:44:10 +0530

    resend-email-verification-token

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -69,6 +69,7 @@ exports.verifyEmail = async (req,res)=>{
     // check if user already verified or not
     if(user.isVerified) return res.json({error:"user is already verified"});
 
+    //check token is present or not
     const token = await EmailVerificationToken.findOne({owner:userId})
     if(!token) return res.json({error:"token not found! "})
 
@@ -102,4 +103,57 @@ exports.verifyEmail = async (req,res)=>{
     })
      
     res.json({message:"Your email is verified."})
+}
+
+exports.resendEmailVerificationToken= async(req,res)=>{
+    const {userId} = req.body;
+
+    const user = await User.findById(userId)
+    if(!user) return res.json({error:"user not found!"})
+
+    if(user.isVerified) return res.json({error:"This email Id is already verified!"})
+
+    const alreadyHasToken = await EmailVerificationToken.findOne({owner: userId});
+    if(alreadyHasToken) return res.json({error:"Only after one hour after you can request for another token!"})
+   
+     // generate 6 digit otp. 
+     let OTP=''
+     for(let i=0;i<=5;i++){
+         // Math.random() * 9 // 4.4234234234
+          let randomNumber  =Math.round(Math.random() * 9 ) // 4
+          OTP+= randomNumber;
+     }
+ 
+     // store otp inside our database.  
+     const newEmailVerificationToken = new EmailVerificationToken({
+         owner:user._id,
+         token:OTP
+     })
+ 
+     await newEmailVerificationToken.save()
+ 
+     // send that otp to our user.
+     // nodemailer webside data
+     const transport = nodemailer.createTransport({
+         host: "sandbox.smtp.mailtrap.io",
+         port: 2525,
+         auth: {
+           user: "596002148130f3",
+           pass: "cebd3db9c31d88"
+         }
+       });
+ 
+       // sending mail for otp 
+       transport.sendMail({
+         from:"verification@imdb.com",
+         to:user.email,
+         subject:'Email Verification',
+         html:`
+         <p>Your Verification OTP</p>
+         <h1>${OTP}</h1>
+         `
+       })
+     
+     res.status(201).json({message:"New OTP has been sent to your registered email account!"})
+
 }
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,10 +1,11 @@
 const express = require('express')
-const { create, verifyEmail } = require('../controllers/user.controller');
+const { create, verifyEmail, resendEmailVerificationToken } = require('../controllers/user.controller');
 const { validate, userValidtor } = require('../middlewares/validator');
 
 const router = express.Router()
 
 router.post('/create',userValidtor,validate,create);
 router.post('/verify-email',verifyEmail);
+router.post('/resend-email-verification-token',resendEmailVerificationToken);
 
 module.exports=router;
