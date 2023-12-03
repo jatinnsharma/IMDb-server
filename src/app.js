a8926b7f01bea7ec4d5fb9e19cb271dab391df6e
jatin <jatinsharma2231@gmail.com>
2023-12-03 14:25:57 +0530

    async error handler without try catch

--- src/app.js
+++ src/app.js
@@ -1,22 +1,26 @@
 require('dotenv').config({path:  './.env'})
+require('express-async-errors');
 const express = require('express')
 const app = express()
 const cors = require('cors')
 var bodyParser = require('body-parser')
 const userRouter = require('./routes/user.route')
 require('./db')
-const morgan = require('morgan')
+const morgan = require('morgan');
+const { errorHandler } = require('./middlewares/errorHandler.middleware');
 
 const PORT = process.env.PORT
 
-
+// middlewares
 app.use(cors())
 app.use(express.json())
 app.use(morgan('dev'))
 app.use(bodyParser.urlencoded({ extended: false }))
 
-
 app.use("/api/user", userRouter);
+
+app.use(errorHandler)
+
 
 app.listen(PORT,()=>{
     console.log(`Server is running on PORT ${PORT}`)
