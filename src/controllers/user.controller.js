efd5591315de7bbe2a31c6c5de7233b4290198f9
jatin <jatinsharma2231@gmail.com>
2023-12-08 15:56:27 +0530

    reading query params

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -155,7 +155,7 @@ exports.forgetPassword = async (req, res) => {
 
   await newPasswordResetToken.save()
 
-  const resetPasswordUrl = `http://localhost:3000/reset-password?token=${token}&id=${user._id}`
+  const resetPasswordUrl = `http://localhost:3000/auth/reset-password?token=${token}&id=${user._id}`
 
   const transport = generateMailTransporter()
 
